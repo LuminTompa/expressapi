f7419ffac05ea8a62f18a0159456f6795d578f77
LuminTompa <lumincob@gmail.com>
2022-08-18 11:39:23 +0200

    lagt till de 4 endpoints

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,22 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/api/games", (req, res) => {
+  res.status(200).json([]);
+});
+
+app.post("/api/games", (req, res) => {
+  res.status(201).json({});
+});
+
+app.put("/api/games/:id", (req, res) => {
+  res.status(200).json({});
+});
+
+app.delete("/api/games/:id", (req, res) => {
+  res.status(204).json({});
+});
+
 app.listen(3000, () => {
   console.log("Server is now running: http://localhost:3000");
 });
